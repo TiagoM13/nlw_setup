f0933e58ee6f04b747dd223e0a3cfdf9105747c2
TiagoM13 <thyagomotha2000@gmail.com>
2023-01-19 22:33:47 -0300

    feat: add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import Fastify from "fastify";
-import { PrismaClient } from 'prisma/prisma-client'
+import { PrismaClient } from "prisma/prisma-client";
+import cors from "@fastify/cors";
 
 const app = Fastify();
 const prisma = new PrismaClient();
+
+app.register(cors)
 
 app.get('/', async () => {
   const habits = await prisma.habit.findMany({
