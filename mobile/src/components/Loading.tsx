43883101d371d9080d17871c98d30d7c543a454b
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-04 13:17:53 -0300

    feat: add tailwindcss and configs

--- mobile/src/components/Loading.tsx
+++ mobile/src/components/Loading.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ActivityIndicator, View } from 'react-native';
 
-// import { Container } from './styles';
-
 export const Loading = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#09090A' }}>
---
+++ mobile/src/screens/Home.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+
+export const Home = () => {
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+    </View>
+  )
+}
