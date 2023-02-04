e3c9d397002a4e54b384c316efead7a98106dbaa
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-03 22:00:53 -0300

    feat: add header componet

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,13 @@
+import { Header } from './components/Header';
 import './styles/global.css';
 
-import { Habit } from './components/Habit';
-
+// import { Habit } from './components/Habit';
 const App = () => {
   return (
-    <div>
-      <Habit completed={3} />
-      <Habit completed={15} />
-      <Habit completed={30} />
-      <Habit completed={60} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
     </div>
   )
 }
