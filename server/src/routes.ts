8f2d09cfa66931819479c52f49f395d3de116ab1
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-10 22:30:15 -0300

    feat: create days summary route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -74,7 +74,6 @@ export async function appRoutes(app: FastifyInstance) {
   });
 
   app.patch('/habits/:id/toggle', async (request) => {
-
     const toggleHabitParams = z.object({
       id: z.string().uuid(),
     })
@@ -120,5 +119,32 @@ export async function appRoutes(app: FastifyInstance) {
         }
       })
     }
+  })
+
+  app.get('/summary', async (reqquest) => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id, 
+        D.date,
+        ( 
+          SELECT
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `
+
+    return summary
   })
 }
