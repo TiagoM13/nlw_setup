8f908037a4dcab8ee4b91f5d6e217ee1cb815134
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-21 11:05:41 -0300

    feat create back button

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import { BackButton } from '../components/BackButton';
 
 export const New = () => {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView showsVerticalScrollIndicator={false}>
 
+        <BackButton />
+
+      </ScrollView>
     </View>
   );
 }
