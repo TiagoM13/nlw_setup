12b3e6e49ec923b7752146311d63e797638a9a7d
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-21 10:56:26 -0300

    feat: create navigation for registration screen

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
 
 import Logo from '../assets/logo.svg';
 
 export const Header = () => {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -13,6 +16,7 @@ export const Header = () => {
       <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-xl items-center"
+        onPress={() => navigate('new')}
       >
         <Feather
           name='plus'
