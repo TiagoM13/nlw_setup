3bf7542be30069b51e18f3f2f4388578c3408531
TiagoM13 <thyagomotha2000@gmail.com>
2023-02-21 10:43:55 -0300

    feat: create routes navigation and route component

---
+++ mobile/src/screens/Habit.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export const Habit = () => {
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+
+    </View>
+  );
+}
