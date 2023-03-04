85f0e8d5b257cd4d64e53f5d4e765c4bae551270
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-04 12:18:48 -0300

    feat: update SummaryTable

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -29,26 +29,25 @@ export const Summarytable = () => {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date => {
+        {summary && summaryDates.map(date => {
+
           const dayInSummary = summary.find(day => {
             return dayjs(date).isSame(day.date, 'day')
           })
+
           return (
             <HabitDay
               key={date.toString()}
               date={date}
               amount={dayInSummary?.amount}
-              completed={dayInSummary?.completed}
+              defaultCompleted={dayInSummary?.completed}
             />
           )
         })}
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
           return (
-            <div
-              key={i}
-              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
-            />
+            <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
           )
         })}
       </div>
