a22621b4198ef8e4f00bbe9d18345a883bbe390c
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-04 22:52:56 -0300

    feat: update all components

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,35 +1,40 @@
-import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
-import { Feather } from '@expo/vector-icons';
-import colors from 'tailwindcss/colors';
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+import { Feather } from '@expo/vector-icons'
+import colors from "tailwindcss/colors";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 
-interface ICheckbox extends TouchableOpacityProps {
+interface ICheckbo extends TouchableOpacityProps {
   title: string;
-  checked?: boolean
+  checked?: boolean;
 }
 
-export const Checkbox = ({ title, checked = false, ...rest }: ICheckbox) => {
+export const Checkbox = ({ title, checked = false, ...rest }: ICheckbo) => {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
       className="flex-row mb-2 items-center"
       {...rest}
     >
-      {
-        checked
-          ?
-          <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
-            <Feather
-              name="check"
-              size={20}
-              color={colors.white}
-            />
-          </View>
-          :
-          <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
+      {checked
+        ?
+        <Animated.View
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
+          <Feather
+            name="check"
+            size={20}
+            color={colors.white}
+          />
+        </Animated.View>
+        :
+        <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       }
 
-      <Text className="text-white text-base ml-3 font-semibold">{title}</Text>
-
+      <Text className="text-white text-base ml-3 font-semibold">
+        {title}
+      </Text>
     </TouchableOpacity>
-  );
+  )
 }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,18 +1,19 @@
-import { useNavigation } from "@react-navigation/native";
-import dayjs from "dayjs";
-import { useEffect, useState } from "react";
-import { View, Text, ScrollView, Alert } from "react-native";
-import { DAY_SIZE, HabitDay } from "../components/HabitDay";
-import { Header } from "../components/Header";
-import { Loading } from "../components/Loading";
+import { useCallback, useState } from 'react';
+import { Text, View, ScrollView, Alert } from 'react-native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
-import { api } from "../lib/axios";
-import { generateDateFromYearBiginning } from '../utils/generate-dates-from-year-biginning';
+import { api } from '../lib/axios';
+import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
+
+import { Header } from '../components/Header';
+import { Loading } from '../components/Loading';
+import { HabitDay, DAY_SIZE } from '../components/HabitDay';
+import dayjs from 'dayjs';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
-const datesFromYearStart = generateDateFromYearBiginning();
-const minimumSUmmaryDatesSizes = 18 * 5;
-const amountOfDaysToFill = minimumSUmmaryDatesSizes - datesFromYearStart.length;
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimunSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 type SummaryProps = Array<{
   id: string;
@@ -22,16 +23,16 @@ type SummaryProps = Array<{
 }>
 
 export const Home = () => {
-  const [loading, setLoading] = useState(true);
-  const [summary, setSummary] = useState<SummaryProps | null>(null);
-  const { navigate } = useNavigation();
+  const [loading, setLoading] = useState(true)
+  const [summary, setSummary] = useState<SummaryProps | null>(null)
+
+  const { navigate } = useNavigation()
 
   async function fetchData() {
     try {
       setLoading(true)
       const response = await api.get('/summary');
       setSummary(response.data)
-      console.log(response.data)
     } catch (error) {
       Alert.alert('Ops', 'Não foi possível carregar o sumário de hábitos.')
       console.log(error)
@@ -40,9 +41,9 @@ export const Home = () => {
     }
   }
 
-  useEffect(() => {
-    fetchData();
-  }, [])
+  useFocusEffect(useCallback(() => {
+    fetchData()
+  }, []))
 
   if (!loading) {
     return (
@@ -51,7 +52,7 @@ export const Home = () => {
   }
 
   return (
-    <View className="flex-1 bg-background px-8 pt-16">
+    <View className='flex-1 bg-background px-8 pt-16'>
       <Header />
 
       <View className="flex-row mt-6 mb-2">
@@ -67,13 +68,13 @@ export const Home = () => {
       </View>
 
       <ScrollView
-        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 100 }}
       >
-        {summary &&
-          <View className="flex-row flex-wrap">
+        {summary && (
+          <View className='flex-row flex-wrap'>
             {
-              datesFromYearStart.map((date => {
+              datesFromYearStart.map(date => {
                 const dayWithHabits = summary.find(day => {
                   return dayjs(date).isSame(day.date, 'day')
                 })
@@ -87,8 +88,7 @@ export const Home = () => {
                     onPress={() => navigate('habit', { date: date.toISOString() })}
                   />
                 )
-              }
-              ))
+              })
             }
 
             {
@@ -97,14 +97,15 @@ export const Home = () => {
                 .map((_, index) => (
                   <View
                     key={index}
-                    className="bg-zinc-900 rounded-lg border-2 border-zinc-800 m-1 opacity-40"
+                    className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
                     style={{ width: DAY_SIZE, height: DAY_SIZE }}
                   />
                 ))
             }
           </View>
-        }
+        )}
       </ScrollView>
+
     </View>
   )
-}
+} 
